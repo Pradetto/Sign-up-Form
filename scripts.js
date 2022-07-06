33f07b3b4d8a4cb804ae03318616b70d36e228f5
Michael Pradetto <pradetto5@gmail.com>
2022-07-05 17:48:51 -0700

    completed signup form

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', () =>{
+    const password = document.querySelector('#password');
+    const confirmPassword = document.querySelector('#confirmPassword');
+    if (password.value !== confirmPassword.value){
+        alert('Passwords do not match please try again')
+    } else {
+        return console.log('Good passwords')
+    };
+})
